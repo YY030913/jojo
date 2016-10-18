5d2c15b31183e4a2fca1cd56afa5a9f60fdbb545
肖雨 <xiaoyu@xiaoyudeMacBook-Air.local>
2016-10-18 18:27:39 +0800

    baidu

--- web/application/views/public/header.php
+++ web/application/views/public/header.php
@@ -18,6 +18,7 @@ echo link_tag('static/css/style.css?v=1.0');
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=yes">
 <meta name="HandheldFriendly" content="true">
+<meta name="baidu-site-verification" content="lnrw6T2nxa" />
 <script>
 var _hmt = _hmt || [];
 (function() {
