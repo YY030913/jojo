fddf2a4b8311c2ffd770dd24528c522401d59a9a
肖雨 <xiaoyu@xiaoyudeMacBook-Air.local>
2016-10-22 03:18:05 +0800

    remove leftsead

--- web/application/views/index.php
+++ web/application/views/index.php
@@ -53,7 +53,7 @@ td,th{ white-space:nowrap;text-overflow:ellipsis; overflow:hidden;max-width:150p
 	</div>
 </div>
 <?php
-load_template('public/leftsead');
+// load_template('public/leftsead');
 load_template('public/js');
 load_template('public/footer');
 ?>
